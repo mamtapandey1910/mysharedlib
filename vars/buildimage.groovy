103a72923911d2aed620d9b5834782a286d04e7b
mamtapandey1910 <loginmamta1910@gmail.com>
2021-09-01 01:37:51 +0530

    Create buildimage.groovy

---
+++ vars/buildimage.groovy
@@ -0,0 +1,10 @@
+import org.jenkinsci.plugins.docker.workflow.*
+import org.jenkinsci.plugins.workflow.cps.CpsScript
+
+
+static builder(script, DSL steps) {
+    // create the image to use in this build instead of using a parameter
+    def docker script.getClass().getClassLoader().loadClass("org.jenkinsci.plugins.docker.workflow.Docker").getConstructor(CpsScript.class).newInstance(script);
+    image = docker.image("hello-world")
+    return new Builder(script, steps, image)
+}
