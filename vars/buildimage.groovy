4511751d0dd98cbae1e16546a002c5b201165016
mamtapandey1910 <loginmamta1910@gmail.com>
2021-09-01 17:20:40 +0530

    Update buildimage.groovy

--- vars/buildimage.groovy
+++ vars/buildimage.groovy
@@ -1,3 +1,4 @@
 static builder(params) {
+    withCredentials([usernamePassword(credentialsId: 'myregistry-login', passwordVariable: 'mamtapandey@587', usernameVariable: 'mamtapandey')])
     sh 'docker build -t ${params} .'
 }
